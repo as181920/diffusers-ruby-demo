5e7e9f8b1087d9d498bede02ef0a8350dccf5a37
Andersen Fan <as181920@gmail.com>
2025-01-07 22:46:42 +0800

    Create randn noise latents

---
+++ ddim_scheduler.rb
@@ -0,0 +1,2 @@
+class DDIMScheduler
+end
--- dreamlike_demo.rb
+++ dreamlike_demo.rb
@@ -3,13 +3,12 @@ require "onnxruntime"
 require "tokenizers"
 require "debug"
 
-if Torch::CUDA.available?
-  device = "cuda"
-  providers = %w[CUDAExecutionProvider]
-else
-  device = "cpu"
-  providers = %w[CPUExecutionProvider]
-end
+providers = if Torch::CUDA.available?
+              %w[CUDAExecutionProvider]
+            else
+              %w[CPUExecutionProvider]
+            end
+device = "cpu" # use "cuda" with gpu, currently use cpu temporarily to handle gpu runtime error
 
 # model:
 # https://huggingface.co/docs/diffusers/tutorials/autopipeline
@@ -21,12 +20,9 @@ end
 
 # Create text tokens
 prompt = ["godzilla is watching kitty doing homework"]
-height = 512
-width = 512
-num_inference_steps = 25 # denoising steps
-guidance_scale = 7.5 # classifier-free guidance
 batch_size = prompt.length
 
+# Create text tokens
 tokenizer = Tokenizers.from_pretrained("openai/clip-vit-base-patch32")
 tokenizer.enable_padding(length: 77, pad_id: 49407)
 tokenizer.enable_truncation(77)
@@ -43,7 +39,7 @@ text_embeddings = Torch.no_grad do
 end
 pp text_embeddings
 
-# generate the unconditional text embeddings which are the embeddings for the padding token.
+# Generate the unconditional text embeddings which are the embeddings for the padding token.
 # max_length = text_ids.shape[-1] # 77
 uncond_tokens = tokenizer.encode_batch([""] * batch_size)
 uncond_ids = Torch.tensor(uncond_tokens.map(&:ids))
@@ -52,10 +48,19 @@ uncond_embeddings = text_encoder.predict({ input_ids: uncond_ids })
 
 text_embeddings = Torch.cat([uncond_embeddings, text_embeddings])
 
+# Create random noise
+height = 512
+width = 512
+unet = OnnxRuntime::Model.new("../onnx/unet/model.onnx", providers:)
+channels_num = unet.inputs.detect{ |e| e[:name] == "sample" }[:shape][1]
+generator = Torch::Generator.new.manual_seed(0) # Seed generator to create the initial latent noise
+latents = Torch.randn([batch_size, channels_num, height / 8, width / 8], generator:, device:) # Shape: 1x4x64x64
+
 # vae_encoder = OnnxRuntime::Model.new("../onnx/vae_encoder/model.onnx", providers:)
 # vae_decoder = OnnxRuntime::Model.new("./onnx/vae_decoder/model.onnx", providers:)
 #
-# unet = OnnxRuntime::Model.new("./onnx/unet/model.onnx", providers:)
 
+num_inference_steps = 25 # denoising steps
+guidance_scale = 7.5 # classifier-free guidance
 
 debugger
